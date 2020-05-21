160bf6a48b0e8f21697288ce822cb3114dc6f769
cc <specialize123@Gmail.com>
2020-05-20 17:47:14 -0700

    Update metric_job.py

--- metric/metric_job.py
+++ metric/metric_job.py
@@ -1,30 +1,12 @@
-"""This script runs jobs in metric folder.
-Please call the following functions as instruction below before executing codes
-in exps folder.
+"""A daily cron job to collect tweets of friends and followers.
 """
 from metric.analysis import Analyzer
-from metric import resolve_url
 
 ### Daily Cron Job ###
 ### Fetches Tweets from connections of bots
 ### Please set the activation time out of the bot activation time range
 ### to avoid Twitter API rate limit.
-# BOTS = ['your_bot_screen_name']
-#analyzer = Analyzer()
-#analyzer.FetchConnTweets(BOTS)
+DRIFTERS = ['your_bot_screen_name']
+analyzer = Analyzer()
+analyzer.FetchConnTweets(DRIFTERS)
 ### End Daily Cron Job ###
-
-### Compute Metric scores for each Tweet ###
-## 1. Build resolved_urls in Tweet table.
-##    It must be called before computing url-based scores.
-#resolve_url.get_expanded_url()
-## 2. Computes url-based score for each tweet
-# analyzer = Analyzer()
-# analyzer.ComputeURLTweetScore()
-## 3. Computes hashtag-based score for each tweet
-# analyzer = Analyzer()
-# analyzer.ComputeHashTagTweetScore()
-## 4. Computes low credibility score for each tweet
-# analyzer = Analyzer()
-# analyzer.ComputeLowCredTweetScore()
-###End Computing Metric scores for each Tweet ###
