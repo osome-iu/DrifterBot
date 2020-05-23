41997e640072f901fc25c582f0a9bb2320ccc768
cc <specialize123@Gmail.com>
2020-05-22 21:45:47 -0700

    Update const_config.py

--- metric/const_config.py
+++ metric/const_config.py
@@ -1,8 +1,6 @@
+# not used in the analysis shown in the paper.
 MIN_VALID_MEDIA_SOURCES = 2
 
+# check the user timeline of tgiven number of friends and followers of each drifter daily.
 NUM_FRIENDS_TO_SAVE = 5
 NUM_FOLLOWERS_TO_SAVE = 5
-
-VALID_DATES_CONN = 21
-
-NUM_SAMPLES = 100
